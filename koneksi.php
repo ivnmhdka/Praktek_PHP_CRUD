8974e3a583e15cb55520173e568ac27f9f683086
ivnmhdka <ivanmahadika05@gmail.com>
2021-10-21 08:42:01 +0700

    First Commit

---
+++ koneksi.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "praktek_php_crud";
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+// if ($conn) {
+//    echo "koneksi berhasil";
+//} else {
+//    echo "koneksi gagal";
+//}
